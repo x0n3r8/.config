157f427dcd0b6d486035d368ce1840c39e58a373
x0n3r8 <31377646+x0n3r8@users.noreply.github.com>
2017-08-26 16:33:33 -0800

    Add files via upload
    My i3wm and i3lock config files

---
+++ lock.sh
@@ -0,0 +1,7 @@
+scrot /tmp/screen.png
+#convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
+#convert /tmp/screen.png -paint 3 /tmp/screen.png
+[[ -f /home/funfinn2/.config/i3/lock.png ]] && convert /tmp/screen.png  /home/funfinn2/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+mocp -P
+i3lock -u -e -i /tmp/screen.png
